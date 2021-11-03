97ae7622bc42a90ba8e54150ea274fcad94823b6
Anosh D. Ullenius <anosh@anosh.se>
2021-11-03 21:49:36 +0100

    Initial commit

---
+++ procent.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// branchless percentual change
+
+void procent(double ny, double old) {
+
+	int result = ny - old;
+	int sign = result >> 31;
+	printf("Sign: %d\n", sign);
+
+	int smaller = sign ^ sign - (-sign); // two's complement form
+	printf("Smaller = %d\n", smaller);
+
+	printf("Smaller reversed... %d\n", smaller ^ 1);
+	const double forandringsfaktor = (double) ny / (double) old;
+	const double procent = ((1 - forandringsfaktor) * smaller) + ( (forandringsfaktor - 1) * (smaller ^ 1) );
+
+	char symbol = (char) (43 + smaller + smaller); // ASCII for +
+	printf("Procent: %c %f\n", symbol, procent * 100);
+}
+
+
+int main() {
+
+	procent(100, 200);
+	procent(300, 250);
+
+    return 0;
+}
