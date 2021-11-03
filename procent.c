2624b70f5947b6e1993d267e8f38cd00eb77f449
Anosh D. Ullenius <anosh@anosh.se>
2021-11-03 22:26:07 +0100

    Minor refactoring

--- procent.c
+++ procent.c
@@ -4,19 +4,19 @@
 
 void procent(double ny, double old) {
 
-	int result = ny - old;
-	int sign = result >> 31;
+	const int diff = ny - old;
+	const int sign = diff >> 31;
 	printf("Sign: %d\n", sign);
 
-	int smaller = sign ^ sign - (-sign); /* two's complement form */
+	const int smaller = sign ^ sign - (-sign); /* two's complement form */
 	printf("Smaller = %d\n", smaller);
 
 	printf("Smaller reversed... %d\n", smaller ^ 1);
 	const double forandringsfaktor = (double) ny / (double) old;
 	const double procent = ((1 - forandringsfaktor) * smaller) + ( (forandringsfaktor - 1) * (smaller ^ 1) );
 
-	char symbol = (char) (43 + smaller + smaller); /* ASCII for + */
-	printf("Procent: %c %f\n", symbol, procent * 100);
+	const char symbol = (char) (43 + smaller + smaller); /* ASCII for + */
+	printf("Procent: %c %.2f\n", symbol, procent * 100);
 }
 
 
