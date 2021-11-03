f1e64ac676165439491cdab2b1d432444352046e
Anosh D. Ullenius <anosh@anosh.se>
2021-11-03 22:08:17 +0100

    Minor. Convert to ANSI C

--- procent.c
+++ procent.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// branchless percentual change
+/* branchless percentual change */
 
 void procent(double ny, double old) {
 
@@ -8,14 +8,14 @@ void procent(double ny, double old) {
 	int sign = result >> 31;
 	printf("Sign: %d\n", sign);
 
-	int smaller = sign ^ sign - (-sign); // two's complement form
+	int smaller = sign ^ sign - (-sign); /* two's complement form */
 	printf("Smaller = %d\n", smaller);
 
 	printf("Smaller reversed... %d\n", smaller ^ 1);
 	const double forandringsfaktor = (double) ny / (double) old;
 	const double procent = ((1 - forandringsfaktor) * smaller) + ( (forandringsfaktor - 1) * (smaller ^ 1) );
 
-	char symbol = (char) (43 + smaller + smaller); // ASCII for +
+	char symbol = (char) (43 + smaller + smaller); /* ASCII for + */
 	printf("Procent: %c %f\n", symbol, procent * 100);
 }
 
