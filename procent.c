a654e6fa60a9f422f929b21e07233f728967162d
Anosh D. Ullenius <anosh@anosh.se>
2022-06-12 22:07:15 +0200

    Use english variable names

--- procent.c
+++ procent.c
@@ -2,9 +2,9 @@
 
 /* branchless percentual change */
 
-void procent(double ny, double old) {
+void percent(double new, double old) {
 
-	const int diff = ny - old;
+	const int diff = new - old;
 	const int sign = diff >> 31;
 	printf("Sign: %d\n", sign);
 
@@ -12,18 +12,17 @@ void procent(double ny, double old) {
 	printf("Smaller = %d\n", smaller);
 
 	printf("Smaller reversed... %d\n", smaller ^ 1);
-	const double forandringsfaktor = (double) ny / (double) old;
-	const double procent = ((1 - forandringsfaktor) * smaller) + ( (forandringsfaktor - 1) * (smaller ^ 1) );
+	const double forandringsfaktor = (double) new / (double) old;
+	const double percent = ((1 - forandringsfaktor) * smaller) + ( (forandringsfaktor - 1) * (smaller ^ 1) );
 
 	const char symbol = (char) (43 + smaller + smaller); /* ASCII for + */
-	printf("Procent: %c %.2f %%\n", symbol, procent * 100);
+	printf("Percentage: %c %.2f %%\n", symbol, percent * 100);
 }
 
 
 int main() {
-
-	procent(100, 200);
-	procent(300, 250);
+	percent(100, 200);
+	percent(300, 250);
 
     return 0;
 }
